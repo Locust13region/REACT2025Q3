4dc60d5df7e868a8fb9c976e96b4ef5ee4cc5f50
DESKTOP-MF8V5D1\Andrey <m>
2025-07-26 12:19:40 +0300

    feat: book description layout

--- rs-react-app/src/components/book-description.tsx
+++ rs-react-app/src/components/book-description.tsx
@@ -1,7 +1,26 @@
 import { type FC } from 'react';
 
 const BookDescription: FC = () => {
-  return <article className="book-description">ARTICLE</article>;
+  return (
+    <article className="book-description">
+      <button>X</button>
+      <h2>Book Title</h2>
+      <h3>Author</h3>
+      <p>book description</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste architecto
+        sint perferendis atque maxime natus neque rerum error exercitationem
+        ratione? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+        cupiditate, consectetur est doloremque repellat ut, quo eos tempore
+        porro modi quis corrupti. Ab quidem veritatis molestiae ex maiores harum
+        optio. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+        magnam odit ipsum aperiam est, harum quam eos mollitia temporibus
+        nostrum neque non deleniti quia cupiditate saepe ipsa earum dolorem
+        recusandae exercitationem magni reiciendis fugiat atque blanditiis
+        totam. Asperiores illo delectus libero ipsum quasi natus mollitia Lorem
+      </p>
+    </article>
+  );
 };
 
 export default BookDescription;
