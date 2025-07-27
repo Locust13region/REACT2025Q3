b3e304b4a1780aadd71c0f9520b20a5838b2f9af
DESKTOP-MF8V5D1\Andrey <m>
2025-07-27 11:37:28 +0300

    feat: caching api requests

--- rs-react-app/src/components/books-list.tsx
+++ rs-react-app/src/components/books-list.tsx
@@ -1,35 +1,32 @@
-import { memo } from 'react';
 import Book from '@/components/book';
 import type { FetchResult } from '@/types/types';
 import Pagination from './pagination';
 
-const BooksList = memo(
-  ({
-    count,
-    next,
-    previous,
-    results: books,
-    searchSubstring,
-  }: FetchResult & {
-    searchSubstring: string;
-  }) => {
-    return (
-      <div className="books-list__wrapper">
-        <ul className="books-list">
-          <li className="books-list__header">
-            <div className="books-list__title">Author</div>
-            <div className="books-list__title">Title</div>
-          </li>
-          {books &&
-            books.map((book) => (
-              <Book key={book.id} {...book} searchSubstring={searchSubstring} />
-            ))}
-        </ul>
-        <Pagination {...{ count, next, previous }} />
-      </div>
-    );
-  }
-);
+const BooksList = ({
+  count,
+  next,
+  previous,
+  results: books,
+  searchSubstring,
+}: FetchResult & {
+  searchSubstring: string;
+}) => {
+  return (
+    <div className="books-list__wrapper">
+      <ul className="books-list">
+        <li className="books-list__header">
+          <div className="books-list__title">Author</div>
+          <div className="books-list__title">Title</div>
+        </li>
+        {books &&
+          books.map((book) => (
+            <Book key={book.id} {...book} searchSubstring={searchSubstring} />
+          ))}
+      </ul>
+      <Pagination {...{ count, next, previous }} />
+    </div>
+  );
+};
 BooksList.displayName = 'BooksList';
 
 export default BooksList;
--- rs-react-app/src/hooks/use-api.ts
+++ rs-react-app/src/hooks/use-api.ts
@@ -2,6 +2,8 @@ import dataFetch from '@/api/api-request';
 import { useEffect, useState } from 'react';
 import type { FetchResult } from '@/types/types';
 
+const cache = new Map<string, FetchResult>();
+
 const useApi = (generatedError: Error | null) => {
   const [loading, setLoading] = useState<boolean>(false);
   const [fetchResult, setFetchResult] = useState<FetchResult>();
@@ -10,10 +12,17 @@ const useApi = (generatedError: Error | null) => {
 
   useEffect(() => {
     const dataRequest = async () => {
+      if (cache.has(requestUrl)) {
+        setFetchResult(cache.get(requestUrl));
+        setLoading(false);
+        return;
+      }
+
       try {
         setLoading(true);
         setFetchError(null);
         const result = await dataFetch(requestUrl);
+        cache.set(requestUrl, result);
         setFetchResult(result);
       } catch (error) {
         if (error instanceof Error) {
