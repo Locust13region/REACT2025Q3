786a62de353460e38bce9091127015da0cd40b61
DESKTOP-MF8V5D1\Andrey <m>
2025-08-13 18:39:09 +0300

    feat: setup next, remove unnecessary dependencies

--- rs-react-app/src/App.tsx
+++ rs-react-app/src/App.tsx
@@ -5,29 +5,42 @@ import ErrorBoundary from './components/error-boundary';
 import { lazy, Suspense } from 'react';
 import BookDescription from './components/book-description';
 
+import { BrowserRouter } from 'react-router';
+import './index.css';
+import ThemeProvider from '@/components/theme-provider.tsx';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+
 const Content = lazy(() => import('@/pages/content'));
 const About = lazy(() => import('@/pages/about'));
 const NotFound = lazy(() => import('@/pages/404'));
 
 export default function App() {
   return (
-    <>
-      <Header></Header>
-      <ErrorBoundary>
-        <Suspense fallback={<h2>Loading page...</h2>}>
-          <Routes>
-            <Route path="/" element={<Navigate to="/books/1" replace />} />
-            <Route path="/books" element={<Navigate to="/books/1" replace />} />
+    <BrowserRouter>
+      <ThemeProvider>
+        <Provider store={store}>
+          <Header></Header>
+          <ErrorBoundary>
+            <Suspense fallback={<h2>Loading page...</h2>}>
+              <Routes>
+                <Route path="/" element={<Navigate to="/books/1" replace />} />
+                <Route
+                  path="/books"
+                  element={<Navigate to="/books/1" replace />}
+                />
 
-            <Route path="/books/:page" element={<Content />}>
-              <Route path=":bookId" element={<BookDescription />} />
-            </Route>
+                <Route path="/books/:page" element={<Content />}>
+                  <Route path=":bookId" element={<BookDescription />} />
+                </Route>
 
-            <Route path="about" element={<About />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Suspense>
-      </ErrorBoundary>
-    </>
+                <Route path="about" element={<About />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </Suspense>
+          </ErrorBoundary>
+        </Provider>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
