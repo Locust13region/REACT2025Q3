0bc460bd41296f8f2c4b1ee67313e6b02e347dc6
DESKTOP-MF8V5D1\Andrey <m>
2025-08-26 12:19:14 +0300

    chore: remove unnecessary code

--- performance/src/App.tsx
+++ performance/src/App.tsx
@@ -2,15 +2,11 @@ import { Provider } from 'react-redux';
 import './App.css';
 
 import { store } from './redux/store';
-import FormsView from './components/form-view/forms-view';
-import Controls from './components/controls/controls';
 
 function App() {
   return (
     <Provider store={store}>
-      <h1>React forms</h1>
-      <Controls />
-      <FormsView />
+      <div>performance</div>
     </Provider>
   );
 }
