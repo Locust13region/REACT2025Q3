57ea756327401084278a5dcb132d33481692713d
DESKTOP-MF8V5D1\Andrey <m>
2025-08-30 10:18:35 +0300

    chore: remove unnecessary dependencies & code

--- performance/src/components/Z/form-uncontrolled/field-error.tsx
+++
@@ -1,7 +0,0 @@
-import type { UncontrolledFieldErrorProps } from '@/types/types';
-
-const FieldError = ({ fieldId, errors }: UncontrolledFieldErrorProps) => {
-  return <p className={`field__error`}>{errors?.[fieldId] || '\u00A0'}</p>;
-};
-
-export default FieldError;
